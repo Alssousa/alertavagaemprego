5e8fa0bdc050d94c135de6c15dafab0d6c312406
Alssousa <alssousa@proton.me>
2025-10-21 18:26:08 -0300

    função new_job implementada -> responsavel pela verificação constante na lista de vagas, caso encontre uma que não esteja contida, a mesma será inserida.

--- main.py
+++ main.py
@@ -6,18 +6,24 @@ from bs4 import BeautifulSoup
 from time import sleep
 
 
-driver = webdriver.Firefox()
 vagas = []
 
 def startup():   
-    driver.get('https://www.infojobs.com.br/empregos.aspx?poblacion=5204517')
+    try:
+        driver = webdriver.Firefox()
+        driver.get('https://www.infojobs.com.br/empregos.aspx?poblacion=5204517')
+    except Exception as e:
+        print("Erro ao iniciar o navegador ou acessar o site:", e)
+        raise e
     sleep(2)
     cookies_btn = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'didomi-notice-agree-button')))
     cookies_btn.click()
     
-    return BeautifulSoup(driver.page_source, 'html.parser')
+    return BeautifulSoup(driver.page_source, 'html.parser'), driver
+    
 
-def list_jobs(html_homepage):
+def list_jobs(html_homepage, driver:webdriver.Firefox):
+    list_vagas = []
     content = html_homepage.select_one('div.js_vacanciesGridFragment')
     print('tamanho: ', len(content), 'type: ', type(content))
     content = content.select('div.js_rowCard')
@@ -34,12 +40,33 @@ def list_jobs(html_homepage):
         
         job_name = job.text.strip()
         empresa_name = (empresa.text.replace('\n', '').strip() if empresa else 'Empresa não informada')
-        vagas.append({'job': job_name, 'empresa': empresa_name, 'modalidade': modalidade, 'local': local, 'feedback': feedback})
+        list_vagas.append({'job': job_name, 'empresa': empresa_name, 'modalidade': modalidade, 'local': local, 'feedback': feedback})
         print('Vaga: ', job_name, ' | Empresa: ', empresa_name, ' | Modalidade: ', modalidade, ' | Local: ', local, ' | Feedback: ', feedback)
-        sleep(1)
-
+        #sleep(1)
     driver.quit()
+    
+    return list_vagas
+
+def new_job():
+    html_homepage, driver = startup()
+    vagas.pop(0)  # remover a primeira vaga para teste
+    print("Buscando novas vagas...")
+    new_vagas = list_jobs(html_homepage, driver)
+    new_vagas.append({'job': 'TECNICO PCM I (PCD)', 'empresa': 'GRUPO    SELPE', 'modalidade': 'Remoto', 'local': 'Imperatriz - MA, 0 Km de você.', 'feedback': '4,4'})
+    print("verificando novas vagas...")
+    for i, new_vaga in enumerate(new_vagas):
+        print(f'vaga {i}: ', new_vaga)
+        #[vagas.append(new_vaga) if new_vaga['job'] not in [vaga['job'] for vaga in vagas] else print(f"Vaga {new_vaga['job']} já cadastrada.")]
+        if new_vaga not in vagas:
+            vagas.append(new_vaga)
+            print(f"\n\nNova vaga cadastrada: {new_vaga['job']}, Empresa: {new_vaga['empresa']}")
+            ################ AÇÃO DE ALERTA PARA NOVA VAGA AQUI ################
+
+    
+    
+    
+    
 # teste 
-html_homepage = startup()
-list_jobs(html_homepage)
-print(vagas)
+html_homepage, driver = startup()
+vagas = list_jobs(html_homepage, driver)
+new_job()
