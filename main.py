c9194714b5866dd8c5b92b87b3bc53e43bdeb826
Alssousa <alssousa@proton.me>
2025-10-21 16:41:13 -0300

    Tratado erros para nome de empresas e feedbacks invalidos e armazenado as informações das vagas em uma lista.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from time import sleep
 
 
 driver = webdriver.Firefox()
+vagas = []
 
 def startup():   
     driver.get('https://www.infojobs.com.br/empregos.aspx?poblacion=5204517')
@@ -26,17 +27,19 @@ def list_jobs(html_homepage):
         modalidade = itens.select('div.d-inline-flex > div')
         modalidade = modalidade[-1].text.strip()
         local = itens.select_one('div.js_cardLink > div.mb-8').text.strip()
-        feedback = itens.select_one('div.mr-8 > div.text-nowrap > span').text.strip()
+        feedback = (itens.select_one('div.mr-8 > div.text-nowrap > span').text.strip() if itens.select_one('div.mr-8 > div.text-nowrap > span') else 'Sem feedback')
         ''' 
             adicionar link da vaga -> pegar um link predefinido com a localização da vaga + &iv=data-id da vaga
         '''
         
         job_name = job.text.strip()
-        empresa_name = empresa.text.replace('\n', '').strip()
-
+        empresa_name = (empresa.text.replace('\n', '').strip() if empresa else 'Empresa não informada')
+        vagas.append({'job': job_name, 'empresa': empresa_name, 'modalidade': modalidade, 'local': local, 'feedback': feedback})
         print('Vaga: ', job_name, ' | Empresa: ', empresa_name, ' | Modalidade: ', modalidade, ' | Local: ', local, ' | Feedback: ', feedback)
-        sleep(5)
+        sleep(1)
 
+    driver.quit()
 # teste 
 html_homepage = startup()
 list_jobs(html_homepage)
+print(vagas)
