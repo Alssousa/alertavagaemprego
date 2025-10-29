23d2ae8a583fa058629a1ce86cfd157e1dae5e8b
Alssousa <alssousa@proton.me>
2025-10-29 00:46:28 -0300

    Implementado threads para o webscraper executar durante o lopping do bot. Adicionado funções controller com tempo para execução do scraper e bot.

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+import threading
+import time
 from web_scraper import startup, list_jobs, new_job
+from telegram_bot import bot, load_user_date, send_alert_new_job
+
+data = load_user_date()
+CHECK_INTERVAL = 300
 
 
+def verify_new_jobs(city:str, user_id):
+    print("Verificando novas vagas...")
+    update = new_job(city)
+    if update:
+        print(f"\n\nNovas vagas encontradas para a cidade {city}: ", update)
+        for vaga in update:
+            send_alert_new_job(user_id, vaga)
+    
 
+def scraper_loop():
+    while True:
+        try:
+            for user_id, item in data.items():
+                city = item['city']
+                if city:
+                    print("Execuntando scraper")
+                    verify_new_jobs(city, user_id)
+        except Exception as e:
+            print("Erro ao executar o loop do scraper..: ", e)
+        time.sleep(CHECK_INTERVAL)
+                    
 
-
-
-
-
-
-
-
-
+######### EXECUTABLE CODE ##########
 
 if __name__ == "__main__":
-    pass
+    t = threading.Thread(target=scraper_loop, daemon=True)
+    t.start()
+    
+    bot.infinity_polling()
+    
