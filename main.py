0283de9787a53f50320893a5a7abd00900d59f51
Alssousa <alssousa@proton.me>
2025-10-30 21:19:42 -0300

    Agora o scraper ocorre com itens de lista de cidades do usuario.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from web_scraper import startup, list_jobs, new_job
 from telegram_bot import bot, load_user_date, send_alert_new_job
 
 data = load_user_date()
-CHECK_INTERVAL = 300
+CHECK_INTERVAL = 2000
 
 
 def verify_new_jobs(city:str, user_id):
@@ -20,10 +20,12 @@ def scraper_loop():
     while True:
         try:
             for user_id, item in data.items():
-                city = item['city']
-                if city:
-                    print("Execuntando scraper")
-                    verify_new_jobs(city, user_id)
+                citys = item['city']
+                print("Dentro 1")
+                if citys:
+                    for city in citys:
+                        print("Execuntando scraper")
+                        verify_new_jobs(city, user_id)
         except Exception as e:
             print("Erro ao executar o loop do scraper..: ", e)
         time.sleep(CHECK_INTERVAL)
@@ -33,7 +35,7 @@ def scraper_loop():
 
 if __name__ == "__main__":
     t = threading.Thread(target=scraper_loop, daemon=True)
-    t.start()
+    #t.start()
     
     bot.infinity_polling()
     
